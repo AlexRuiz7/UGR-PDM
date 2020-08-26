b407210b114bac2db0bf9e7cf2b8fb04772260e4
Álex <alex-r-b@hotmail.com>
2020-08-26 20:00:16 +0200

    APP:GranadaTour - Añadido spinner de carga

--- GranadaTour/app/src/main/java/ugr/pdm/granadatour/MapActivity.java
+++ GranadaTour/app/src/main/java/ugr/pdm/granadatour/MapActivity.java
@@ -1,8 +1,17 @@
 package ugr.pdm.granadatour;
 
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.PointF;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,30 +21,48 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.mapbox.android.core.permissions.PermissionsListener;
+import com.mapbox.android.core.permissions.PermissionsManager;
 import com.mapbox.geojson.Feature;
 import com.mapbox.geojson.FeatureCollection;
 import com.mapbox.geojson.Point;
 import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.annotations.BubbleLayout;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.location.LocationComponent;
+import com.mapbox.mapboxsdk.location.LocationComponentActivationOptions;
+import com.mapbox.mapboxsdk.location.LocationComponentOptions;
+import com.mapbox.mapboxsdk.location.modes.CameraMode;
+import com.mapbox.mapboxsdk.location.modes.RenderMode;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 import com.mapbox.mapboxsdk.maps.Style;
+import com.mapbox.mapboxsdk.style.layers.Property;
 import com.mapbox.mapboxsdk.style.layers.PropertyFactory;
 import com.mapbox.mapboxsdk.style.layers.SymbolLayer;
 import com.mapbox.mapboxsdk.style.sources.GeoJsonSource;
 
+import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 import ugr.pdm.granadatour.models.Item;
 import ugr.pdm.granadatour.models.Place;
+import ugr.pdm.granadatour.utils.State;
 
+import static com.mapbox.mapboxsdk.style.expressions.Expression.eq;
+import static com.mapbox.mapboxsdk.style.expressions.Expression.get;
+import static com.mapbox.mapboxsdk.style.expressions.Expression.literal;
 import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconAllowOverlap;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconAnchor;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconImage;
 import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconOffset;
 
 
-public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback, PermissionsListener, MapboxMap.OnMapClickListener {
 
     private static final String SOURCE_ID = "SOURCE_ID";
     private static final String ICON_ID = "ICON_ID";
@@ -44,8 +71,15 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     private MapView mapView;
     private List<Feature> mPlacesList = new ArrayList<>();
 
+    private static final String CALLOUT_LAYER_ID = "CALLOUT_LAYER_ID";
+    private static final String PROPERTY_SELECTED = "selected";
+    private static final String PROPERTY_NAME = "name";
+    /* Para la localización del dipositivo */
+    private PermissionsManager permissionsManager;
+    private MapboxMap mapboxMap;
+    private GeoJsonSource source;
+
     /**
-     *
      * @param savedInstanceState
      */
     @Override
@@ -94,12 +128,16 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
                     double latitude = Double.parseDouble(placeCoords.split(",")[0]);
                     double longitude = Double.parseDouble(placeCoords.split(",")[1]);
 
-                    mPlacesList.add(Feature.fromGeometry(Point.fromLngLat(longitude, latitude)));
+                    Feature marker = Feature.fromGeometry(Point.fromLngLat(longitude, latitude));
+                    marker.addStringProperty(PROPERTY_NAME, place.getTitle());
+                    marker.addBooleanProperty(PROPERTY_SELECTED, false);
+
+                    mPlacesList.add(marker);
                 }
 
                 @Override
                 public void onCancelled(@NonNull DatabaseError error) {
-                    Log.e("ERROR",  error.getMessage());
+                    Log.e("ERROR", error.getMessage());
                 }
             };
 
@@ -110,37 +148,317 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
 
 
     /**
-     *
      * @param mapboxMap
      */
     @Override
-    public void onMapReady(@NonNull MapboxMap mapboxMap) {
+    public void onMapReady(@NonNull final MapboxMap mapboxMap) {
 
-        mapboxMap.setStyle(new Style.Builder().fromUri("mapbox://styles/mapbox/cjf4m44iw0uza2spb3q0a7s41")
+        String mapStyle;
+        if (State.getInstance().isDarkMode()) {
+            mapStyle = Style.DARK;
+        } else {
+            mapStyle = Style.MAPBOX_STREETS;
+        }
+
+        mapboxMap.setStyle(mapStyle, new Style.OnStyleLoaded() {
+            @Override
+            public void onStyleLoaded(@NonNull Style style) {
+                enableLocationComponent(style);
 
                 // Añadir le estilo del marcador
-                .withImage(ICON_ID, BitmapFactory.decodeResource(
+                style.addImage(ICON_ID, BitmapFactory.decodeResource(
                         MapActivity.this.getResources(), com.mapbox.mapboxsdk.R.drawable.mapbox_marker_icon_default)
-                )
+                );
+
                 // Información de los marcadores en formato GeoJson
-                .withSource(new GeoJsonSource(SOURCE_ID,
-                        FeatureCollection.fromFeatures(mPlacesList))
-                )
+                source = new GeoJsonSource(SOURCE_ID, FeatureCollection.fromFeatures(mPlacesList));
+                style.addSource(source);
+
                 // Dibujar el marcador
-                .withLayer(new SymbolLayer(LAYER_ID, SOURCE_ID)
+                style.addLayer(new SymbolLayer(LAYER_ID, SOURCE_ID)
                         .withProperties(PropertyFactory.iconImage(ICON_ID),
                                 iconAllowOverlap(true),
-                                iconOffset(new Float[] {0f, -9f}))
-                )
-        );
+                                iconOffset(new Float[]{0f, -9f}))
+                );
+
+                // Añadir capa de información. El nombre de la característica es usado como clave
+                // para iconImage
+                style.addLayer(new SymbolLayer(CALLOUT_LAYER_ID, SOURCE_ID)
+                        .withProperties(
+                                iconImage("{name}"),
+                                iconAnchor(Property.ICON_ANCHOR_BOTTOM),
+                                iconAllowOverlap(true),
+                                iconOffset(new Float[]{-2f, -28f})
+                        )
+                        .withFilter(eq((get(PROPERTY_SELECTED)), literal(true)))
+                );
+
+                mapboxMap.addOnMapClickListener(MapActivity.this);
+            }
+        });
+        this.mapboxMap = mapboxMap;
     }
 
-    /** Override de métodos necesario por parte de MapBox **/
+    @Override
+    public boolean onMapClick(@NonNull LatLng point) {
+        return handleClickIcon(mapboxMap.getProjection().toScreenLocation(point));
+    }
+
+    /**
+     * Updates the display of data on the map after the FeatureCollection has been modified
+     */
+    private void refreshSource() {
+        if (source != null && mPlacesList != null) {
+            source.setGeoJson(FeatureCollection.fromFeatures(mPlacesList));
+        }
+    }
+
+    /**
+     * This method handles click events for SymbolLayer symbols.
+     * <p>
+     * When a SymbolLayer icon is clicked, we moved that feature to the selected state.
+     * </p>
+     *
+     * @param screenPoint the point on screen clicked
+     */
+    private boolean handleClickIcon(PointF screenPoint) {
+        List<Feature> features = mapboxMap.queryRenderedFeatures(screenPoint, LAYER_ID);
+        if (!features.isEmpty()) {
+            String name = features.get(0).getStringProperty(PROPERTY_NAME);
+            if (mPlacesList != null) {
+                for (Feature feature : mPlacesList) {
+                    if (feature.getStringProperty(PROPERTY_NAME).equals(name)) {
+                        if (featureSelectStatus(feature)) {
+                            setFeatureSelectState(feature, false);
+                        } else {
+                            setSelected(feature);
+                        }
+                        Log.e("MARKER SELECTED", feature.properties().toString());
+                        new GenerateViewIconTask(MapActivity.this)
+                                .execute(FeatureCollection.fromFeature(feature));
+                    }
+                }
+            }
+        }
+        return !features.isEmpty();
+    }
+
+    /**
+     * Set a feature selected state.
+     *
+     * @param feature the selected feature
+     */
+    private void setSelected(Feature feature) {
+        if (mPlacesList != null) {
+            setFeatureSelectState(feature, true);
+            refreshSource();
+        }
+    }
+
+    /**
+     * Selects the state of a feature
+     *
+     * @param feature the feature to be selected.
+     */
+    private void setFeatureSelectState(Feature feature, boolean selectedState) {
+        if (feature.properties() != null) {
+            feature.properties().addProperty(PROPERTY_SELECTED, selectedState);
+            refreshSource();
+        }
+    }
+
+    /**
+     * Checks whether a Feature's boolean "selected" property is true or false
+     *
+     * @param feature the specific Feature's in the mPlacesList's list of Features.
+     * @return true if "selected" is true. False if the boolean property is false.
+     */
+    private boolean featureSelectStatus(Feature feature) {
+        if (mPlacesList == null) {
+            return false;
+        }
+        return feature.getBooleanProperty(PROPERTY_SELECTED);
+    }
+
+
+    /**
+     * Invoked when the bitmaps have been generated from a view.
+     */
+    public void setImageGenResults(final HashMap<String, Bitmap> imageMap) {
+        if (mapboxMap != null) {
+            mapboxMap.getStyle(new Style.OnStyleLoaded() {
+                @Override
+                public void onStyleLoaded(@NonNull Style style) {
+                    // calling addImages is faster as separate addImage calls for each bitmap.
+                    style.addImages(imageMap);
+                }
+            });
+        }
+    }
+
+    /**
+     * GEOLOCALIZACIÓN
+     * <p>
+     * Ejemplo:
+     * https://docs.mapbox.com/android/maps/examples/location-component-basic-pulsing/
+     * Docs:
+     * https://docs.mapbox.com/android/maps/overview/location-component/
+     *
+     * @param loadedMapStyle
+     */
+    @SuppressWarnings("MissingPermission")
+    private void enableLocationComponent(Style loadedMapStyle) {
+        // Check if permissions are enabled and if not request
+        if (PermissionsManager.areLocationPermissionsGranted(this)) {
+
+            // Enable the most basic pulsing styling by ONLY using
+            // the `.pulseEnabled()` method
+            LocationComponentOptions customLocationComponentOptions = LocationComponentOptions.builder(this)
+                    .pulseEnabled(true)
+                    .build();
+
+            // Get an instance of the component
+            LocationComponent locationComponent = mapboxMap.getLocationComponent();
+
+            // Activate with options
+            locationComponent.activateLocationComponent(
+                    LocationComponentActivationOptions.builder(this, loadedMapStyle)
+                            .locationComponentOptions(customLocationComponentOptions)
+                            .build());
+
+            locationComponent.setLocationComponentEnabled(true);
+
+            // Set the component's camera mode
+            locationComponent.setCameraMode(CameraMode.NONE);
+
+            // Set the component's render mode
+            locationComponent.setRenderMode(RenderMode.COMPASS);
+        } else {
+            permissionsManager = new PermissionsManager(this);
+            permissionsManager.requestLocationPermissions(this);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        permissionsManager.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    public void onExplanationNeeded(List<String> permissionsToExplain) {
+        Toast.makeText(this, R.string.user_location_permission_explanation, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onPermissionResult(boolean granted) {
+        if (granted) {
+            mapboxMap.getStyle(new Style.OnStyleLoaded() {
+                @Override
+                public void onStyleLoaded(@NonNull Style style) {
+                    enableLocationComponent(style);
+                }
+            });
+        } else {
+            Toast.makeText(this, R.string.user_location_permission_not_granted, Toast.LENGTH_LONG).show();
+            finish();
+        }
+    }
+
+    /**
+     * Override de métodos necesario por parte de MapBox
+     **/
 
     @Override
     protected void onStart() {
         super.onStart();
         mapView.onStart();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mapboxMap != null) {
+            mapboxMap.removeOnMapClickListener(this);
+        }
+        mapView.onDestroy();
+    }
+
+    /**
+     * AsyncTask to generate Bitmap from Views to be used as iconImage in a SymbolLayer.
+     * <p>
+     * Call be optionally be called to update the underlying data source after execution.
+     * </p>
+     * <p>
+     * Generating Views on background thread since we are not going to be adding them to the view hierarchy.
+     * </p>
+     */
+    private static class GenerateViewIconTask extends AsyncTask<FeatureCollection, Void, HashMap<String, Bitmap>> {
+
+        private final WeakReference<MapActivity> activityRef;
+        private final boolean refreshSource;
+
+        GenerateViewIconTask(MapActivity activity, boolean refreshSource) {
+            this.activityRef = new WeakReference<>(activity);
+            this.refreshSource = refreshSource;
+        }
+
+        GenerateViewIconTask(MapActivity activity) {
+            this(activity, false);
+        }
+
+        @SuppressWarnings("WrongThread")
+        @Override
+        protected HashMap<String, Bitmap> doInBackground(FeatureCollection... params) {
+            MapActivity activity = activityRef.get();
+
+            if (activity != null) {
+                HashMap<String, View> viewMap = new HashMap<>();
+                HashMap<String, Bitmap> imagesMap = new HashMap<>();
+                LayoutInflater inflater = LayoutInflater.from(activity);
+
+                FeatureCollection featureCollection = params[0];
+
+                for (Feature feature : featureCollection.features()) {
+                    BubbleLayout bubbleLayout = (BubbleLayout)
+                            inflater.inflate(R.layout.symbol_layer_info_window_layout_callout, null);
+
+                    String name = feature.getStringProperty(PROPERTY_NAME);
+                    TextView titleTextView = bubbleLayout.findViewById(R.id.info_window_title);
+                    titleTextView.setText(name);
+
+//                    String style = feature.getStringProperty(PROPERTY_CAPITAL);
+//                    TextView descriptionTextView = bubbleLayout.findViewById(R.id.info_window_description);
+//                    descriptionTextView.setText("This is a description");
+
+                    int measureSpec = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED);
+                    bubbleLayout.measure(measureSpec, measureSpec);
+
+                    float measuredWidth = bubbleLayout.getMeasuredWidth();
+
+                    bubbleLayout.setArrowPosition(measuredWidth / 2 - 5);
+
+                    Bitmap bitmap = SymbolGenerator.generate(bubbleLayout);
+                    imagesMap.put(name, bitmap);
+                    viewMap.put(name, bubbleLayout);
+                }
+
+                return imagesMap;
+            } else {
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(HashMap<String, Bitmap> bitmapHashMap) {
+            super.onPostExecute(bitmapHashMap);
+            MapActivity activity = activityRef.get();
+            if (activity != null && bitmapHashMap != null) {
+                activity.setImageGenResults(bitmapHashMap);
+                if (refreshSource) {
+                    activity.refreshSource();
+                }
+            }
+        }
     }
 
 
@@ -174,20 +492,30 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         mapView.onLowMemory();
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        mapView.onDestroy();
+    /**
+     * Utility class to generate Bitmaps for Symbol.
+     */
+    private static class SymbolGenerator {
+
+        /**
+         * Generate a Bitmap from an Android SDK View.
+         *
+         * @param view the View to be drawn to a Bitmap
+         * @return the generated bitmap
+         */
+        static Bitmap generate(@NonNull View view) {
+            int measureSpec = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED);
+            view.measure(measureSpec, measureSpec);
+
+            int measuredWidth = view.getMeasuredWidth();
+            int measuredHeight = view.getMeasuredHeight();
+
+            view.layout(0, 0, measuredWidth, measuredHeight);
+            Bitmap bitmap = Bitmap.createBitmap(measuredWidth, measuredHeight, Bitmap.Config.ARGB_8888);
+            bitmap.eraseColor(Color.TRANSPARENT);
+            Canvas canvas = new Canvas(bitmap);
+            view.draw(canvas);
+            return bitmap;
+        }
     }
-
 }
-
-
-
-//                mapboxMap.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() {
-//                    @Override
-//                    public void onStyleLoaded(@NonNull Style style) {
-//                        // Map is set up and the style has loaded. Now you can add data or make other map adjustments
-//
-//                    }
-//                });
